fa4122e6c727649d2d9facb92a4e61485e03c158
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2016-09-18 00:06:00 +0900

    Initial commit

---
+++ app/src/main/java/io/github/kobakei/anago/SignInActivity.java
@@ -0,0 +1,13 @@
+package io.github.kobakei.anago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity);
+    }
+}
