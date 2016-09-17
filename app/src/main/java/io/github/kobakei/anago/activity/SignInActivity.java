5cb1bb5c3fe0c21f2dcee6c54c3f9ac9faba94b9
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2016-09-18 00:40:36 +0900

    Enable data binding

--- app/src/main/java/io/github/kobakei/anago/activity/SignInActivity.java
+++ app/src/main/java/io/github/kobakei/anago/activity/SignInActivity.java
@@ -1,15 +1,20 @@
 package io.github.kobakei.anago.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import io.github.kobakei.anago.R;
+import io.github.kobakei.anago.databinding.SignInActivityBinding;
 
+/**
+ * サインイン画面
+ */
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in_activity);
+        SignInActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.sign_in_activity);
     }
 }
